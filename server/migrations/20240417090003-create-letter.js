e7840587f5bc43797215103385574c7684f59094
iqbal1704 <iqbalmipa17@gmail.com>
2024-04-19 14:47:42 +0800

    up to date

--- server/migrations/20240417090003-create-letter.js
+++ server/migrations/20240417090003-create-letter.js
@@ -4,49 +4,38 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Letters', {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
       name: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       released: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       background_image: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       rating: {
-        allowNull: false,
         type: Sequelize.FLOAT
       },
       genre: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       imgUrl_1: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       imgUrl_2: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       imgUrl_3: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       imgUrl_4: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       price: {
-        allowNull: false,
         type: Sequelize.INTEGER
       },
       createdAt: {
